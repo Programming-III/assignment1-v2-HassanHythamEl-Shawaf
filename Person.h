20f63b643eb7a0ba9169d4a276f9ed9ce6fe8e2f
HassanHythamEl-Shawaf <hassan.elshawaf@student.giu-uni.de>
2025-11-11 09:56:29 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,20 +1,20 @@
 #ifndef PERSON_H
 #define PERSON_H
-
 #include <string>
 using namespace std;
 
-//#write Person class here
-
-
-
-
-
-
-
-
-
-
-
-
+class Person {
+private:
+	string name;
+	int id;
+public:
+	Person();
+	Person(string name, int id);
+	~Person();
+	void display();
+	string getName();
+	void setName(string name);
+	int getId();
+	void setId(int id);
+};
 #endif
